8cfb48e040532a766a6837a5c31aa5f9abf37ef0
gbui1993 <gbui@eagles.ewu.edu>
2015-05-06 14:16:45 -0700

    Buttons work/ 12 Enemies Added

---
+++ CSCD349Team1/Assets/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log ("Level Load: "+ name);
+		Application.LoadLevel (name);
+		
+		//GameManager.Instance.SetGameState (GameManager.GameState.ChooseCharacterScene);
+	}
+
+	public void Quit(){
+
+		Application.Quit ();
+
+	}
+}
+
--- CSCD349Team1/Assets/btnStart.cs
+++ CSCD349Team1/Assets/btnStart.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class btnStart : MonoBehaviour {
 
-	public void LoadLevel() {
+	public void LoadLevel(string name) {
 		print ("btn pressed");
+		Debug.Log (name);
 		//GameManager.Instance.SetGameState (GameManager.GameState.ChooseCharacterScene);
 	}
 }
