29ad23df70613957fdcb009535b4dedcabe5e520
gbui1993 <gbui@eagles.ewu.edu>
2015-05-08 10:57:03 -0700

    started setting up the character creation scene

---
+++ CSCD349Team1/Assets/CharacterManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CharacterManager : MonoBehaviour {
+
+	private SpriteRenderer spriterRenderer;
+	public Sprite[] sprites;
+
+	void Awake() {
+		spriterRenderer = FindObjectOfType<SpriteRenderer> ();
+	}
+
+	public void LoadCharacter(int index) {
+
+		spriterRenderer.sprite = sprites [index];
+		print (sprites [index].texture.ToString ());
+	}
+}
