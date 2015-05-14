e46f0516b3b3c9c1b172f3396fde8d0a956fd15c
tonymoua <tonymoua@live.com>
2015-05-14 15:23:42 -0700

    Good/Bad Character Weapons

---
+++ cscd349FinalProject/cscd349FinalProject/Weapons/WeaponElfBow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cscd349FinalProject.Weapons
+{
+    class WeaponStaff : IWeapon
+    {
+        public WeaponStaff(int minDamage, int maxDamage, string name)
+        {
+            minDamage = 20;
+            maxDamage = 85;
+            name = "Bow and Arrow";
+        }
+    }
+}
